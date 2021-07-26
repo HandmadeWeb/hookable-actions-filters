c13ee43f4b8655e5fc4d79d23976e0df7bf95a47
Michael Rook <michaelr0@users.noreply.github.com>
2021-07-26 02:37:07 +0000

    Apply fixes from StyleCI

--- tests/ActionTest.php
+++ tests/ActionTest.php
@@ -2,9 +2,9 @@
 
 namespace Michaelr0\ActionsAndFilters\Tests;
 
-use Orchestra\Testbench\TestCase;
 use Michaelr0\HookableActionsAndFilters\Action;
 use Michaelr0\HookableActionsAndFilters\Providers\HookableActionsAndFiltersServiceProvider;
+use Orchestra\Testbench\TestCase;
 
 class ActionTest extends TestCase
 {
